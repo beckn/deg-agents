655c4d70c10a443afb94d8eb8f1a636daac531b9
tsuAquila <reenacottage@gmail.com>
2025-05-13 23:53:37 +0530

    initial commit

--- app/agents/customer/SolarizationAgent/tools.py
+++
@@ -1,8 +0,0 @@
-from crewai.tools import tool
-
-@tool
-def get_location(id: str) -> str:
-    """
-    Fetches the location of the user based on their ID.
-    """
-    return "Singapore"
--- app/chat_context_store.py
+++
@@ -1,3 +0,0 @@
-class ChatContextStore:
-    def __init__(self, client_id):
-        self.client_id = client_id
